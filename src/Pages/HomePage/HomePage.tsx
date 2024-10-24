fe29963ded058a8f0c378101ce1b4bc7ec107004
YashAgarwal1201 <legoyashx@gmail.com>
2024-10-24 18:51:41 +0530

    feat: home page

--- src/Pages/HomePage/HomePage.tsx
+++ src/Pages/HomePage/HomePage.tsx
@@ -1,75 +1,38 @@
-import { useRef } from "react";
+import { Card } from "primereact/card";
 import Layout from "../../Layout/Layout";
-// import { useAppContext } from "../../Services/AppContext";
 import "./HomePage.scss";
-import { ContextMenu } from "primereact/contextmenu";
-import ScreenColorDepth from "../../Components/WebAPIsComponents/ScreenColorDepth/ScreenColorDepth";
-import ScreenResolution from "../../Components/WebAPIsComponents/ScreenResolution/ScreenResolution";
-import ScreenOrientation from "../../Components/WebAPIsComponents/ScreenOrientation/ScreenOrientation";
-import CookieStatus from "../../Components/WebAPIsComponents/CookiesEnabled/CookiesEnabled";
-import CurrentTime from "../../Components/WebAPIsComponents/CurrentTime/CurrentTime";
-import CurrentDate from "../../Components/WebAPIsComponents/CurrentDate/CurrentDate";
-import TimeZone from "../../Components/WebAPIsComponents/TimeZone/TimeZone";
-import BatteryStatus from "../../Components/WebAPIsComponents/BatteryStatus/BatteryStatus";
-import SpeechRecognitionSupport from "../../Components/WebAPIsComponents/SpeechRecognition/SpeechRecognition";
-import SpeechSynthesisSupport from "../../Components/WebAPIsComponents/SpeechSynthesis/SpeechSynthesis";
-import HdrSupportCheck from "../../Components/WebAPIsComponents/HdrSupport/HdrSupport";
-import ColorGamutSupport from "../../Components/WebAPIsComponents/ColorGamut/ColorGamut";
-import WindowSize from "../../Components/WebAPIsComponents/BrowserWindowSize/BrowserWindowSize";
-import ThemePreference from "../../Components/WebAPIsComponents/DeviceThemePreference/DeviceThemePreference";
-import BrowserDetection from "../../Components/WebAPIsComponents/BrowserDetection/BrowserDetection";
-import OsDetection from "../../Components/WebAPIsComponents/UserOS/UserOS";
-// import { useNavigate } from "react-router-dom";
+import { startTransition } from "react";
+import { useNavigate } from "react-router-dom";
 
 const LandingPage = () => {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
   // const { showToast } = useAppContext();
-
-  const cm = useRef<ContextMenu>(null);
-  const items = [
-    {
-      label: "Reload Project",
-      icon: <span className="pi pi-refresh"></span>,
-      command: () => window?.location?.reload(),
-    },
-    // {
-    //   label: "Change Wallpaper",
-    //   icon: <span className="material-symbols-rounded">wallpaper</span>,
-    //   command: () =>
-    //     showToast("info", "Info", "This feature is under construction."),
-    // },
-  ];
-
-  const cardStylesBase = "h-[300px] bg-color4 text-color1 rounded-md";
 
   return (
     <Layout>
-      {/* <ContextMenu model={items} ref={cm} /> */}
-      <div
-        className="hidden custom-scrollbar w-full h-full p-1 xs:p-2 md:p-3 lg:p-4 bg-color2 rounded-xl border-2 border-color2 grid grid-flow-row gap-2 grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 overflow-y-auto"
-        // onContextMenu={(e: any) => cm.current?.show(e)}
-      >
-        <ScreenColorDepth baseStyle={cardStylesBase} />
-        <ScreenResolution baseStyle={cardStylesBase} />
-        <ScreenOrientation baseStyle={cardStylesBase} />
-        <WindowSize baseStyle={cardStylesBase} />
-
-        <CookieStatus baseStyle={cardStylesBase} />
-
-        <CurrentTime baseStyle={cardStylesBase} />
-        <CurrentDate baseStyle={cardStylesBase} />
-        <TimeZone baseStyle={cardStylesBase} />
-        <BatteryStatus baseStyle={cardStylesBase} />
-
-        <SpeechRecognitionSupport baseStyle={cardStylesBase} />
-        <SpeechSynthesisSupport baseStyle={cardStylesBase} />
-
-        <HdrSupportCheck baseStyle={cardStylesBase} />
-        <ColorGamutSupport baseStyle={cardStylesBase} />
-        <ThemePreference baseStyle={cardStylesBase} />
-
-        <BrowserDetection baseStyle={cardStylesBase} />
-        <OsDetection baseStyle={cardStylesBase} />
+      <div className="custom-scrollbar w-full h-full p-2 md:p-3 lg:p-4 flex flex-col gap-y-4 sm:pa-y-6 md:gap-y-10 overflow-y-auto">
+        <h1 className="text-2xl xs:text-3xl mdl:text-4xl text-color5 font-heading select-none">
+          Project blackcurrant
+        </h1>
+        <div className="w-full h-full flex justify-center items-center">
+          <div className="w-full sm:w-[90%] md:w-[640px] aspect-square grid grid-cols-2 gap-4">
+            {[
+              { title: "Play with Strings", link: "/string-manipulation" },
+              { title: "Play with Numbers", link: "/number" },
+              { title: "Web APIs", link: "/web-apis" },
+            ]?.map((value, key) => (
+              <Card
+                key={key}
+                onClick={() => startTransition(() => navigate(value?.link))}
+                className="bg-color2 flex justify-center items-center cursor-pointer"
+              >
+                <h2 className="font-subHeading text-center text-color5 text-xl xs:text-2xl mdl:text-3xl">
+                  {value?.title}
+                </h2>
+              </Card>
+            ))}
+          </div>
+        </div>
       </div>
     </Layout>
   );
